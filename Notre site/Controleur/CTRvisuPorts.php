20c133a83b9bc028be8962286c51a890fb74673e
dorianfarkas1 <128363936+dorianfarkas1@users.noreply.github.com>
2023-03-24 16:41:41 +0100

    1

---
+++ Notre site/Controleur/CTRvisuPorts.php
@@ -0,0 +1,9 @@
+<?php
+if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
+    $racine="..";
+}
+
+// appel du script de vue qui permet de gerer l'affichage des donnees
+$titre = "Affichage des ports";
+include "$racine/vue/visuPorts.php";
+?>
