b12b5fe812402a05c849b18af280d3caa6dfe60c
dorianfarkas1 <128363936+dorianfarkas1@users.noreply.github.com>
2023-03-24 16:36:10 +0100

    1

--- Notre site/Controleur/CTRpresentation.php
+++ Notre site/Controleur/CTRpresentation.php
@@ -5,7 +5,5 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
 
 // appel du script de vue qui permet de gerer l'affichage des donnees
 $titre = "Acceuil du site";
-include "$racine/vue/bandeau.php";
 include "$racine/vue/presentation.php";
-include "$racine/vue/pied_page.php";
 ?>
