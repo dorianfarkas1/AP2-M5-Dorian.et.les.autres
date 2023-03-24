3a68cdbe9c554eee002217b2ecac1f15d65099bc
dorianfarkas1 <128363936+dorianfarkas1@users.noreply.github.com>
2023-03-24 15:36:32 +0100

    1

--- Notre site/index.php
+++ Notre site/index.php
@@ -1,7 +1,7 @@
 <?php
 include "getRacine.php";
 include "$racine/controleur/controleurPrincipal.php";
-include_once "$racine/modele/authentification.inc.php"; // pour pouvoir utiliser isLoggedOn()
+include_once "$racine/Modele/authentification.inc.php"; // pour pouvoir utiliser isLoggedOn()
 
 if (isset($_GET["action"])){
     $action = $_GET["action"];
