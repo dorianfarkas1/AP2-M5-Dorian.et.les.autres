15b6af4a303dd39b33e54f00ea6cba6790a8ac18
dorianfarkas1 <128363936+dorianfarkas1@users.noreply.github.com>
2023-03-27 10:52:22 +0200

    1

--- Notre site/Controleur/CTRvisuPorts.php
+++ Notre site/Controleur/CTRvisuPorts.php
@@ -2,13 +2,9 @@
 if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
-<?php
-	$SQL = "SELECT * FROM port";
-	$stmt = $connexion->prepare($SQL);
-	$stmt->execute(array()); // on passe dans le tableaux les paramètres si il y en a à fournir (aucun ici)
-	$lesPorts = $stmt->fetchAll();
-?>
+include_once "$racine/Modele/bd.port.inc.php";
 
+$lesPorts = getPorts();
 // appel du script de vue qui permet de gerer l'affichage des donnees
 $titre = "Affichage des ports";
 include "$racine/vue/visuPorts.php";
