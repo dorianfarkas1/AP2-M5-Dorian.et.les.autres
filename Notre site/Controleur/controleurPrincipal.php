ccc243077d49f1ff6e7505ab6638064b96143c19
dorianfarkas1 <128363936+dorianfarkas1@users.noreply.github.com>
2023-04-03 10:30:50 +0200

    1

--- Notre site/Controleur/controleurPrincipal.php
+++ Notre site/Controleur/controleurPrincipal.php
@@ -6,13 +6,8 @@ function controleurPrincipal(string $action) : string {
     $lesActions["afficheBateau"] = "CTRvisuBateau.php";
     $lesActions["affichePort"] = "CTRvisuPorts.php";
     $lesActions["afficheSecteur"] = "CTRvisuSecteurs.php";
-<<<<<<< HEAD
     $lesActions["liaison"] = "CTRliaison.php";
     $lesActions["afficheTraversee"] = "CTRtrave.php";
-=======
-    $lesActions["afficheLiaison"] = "CTRliaison.php";
-    $lesActions["afficheTraversee"] = "CTRtraversee.php";
->>>>>>> a1743691a3043cf5cf8e40c385f15e4d58ee1340
     $lesActions["afficheTarif"] = "CTRtarifs.php";
     $lesActions["modifieBateau"] = "CTRcrudBateau.php";
     $lesActions["bateauTraitement"] = "CTRcrudBateau/crudBateauTraitement.php";
