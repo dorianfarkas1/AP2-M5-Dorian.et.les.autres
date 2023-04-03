a2a09c99909a947d51ba2b1c1b75e03664717bef
Judith hedible <judith.hedible@clsi.fr>
2023-04-03 20:15:10 +0200

    jhjh

--- Notre site/Modele/bd.liaison.inc.php
+++ Notre site/Modele/bd.liaison.inc.php
@@ -1,24 +1,20 @@
 <?php
 include_once "bd.inc.php";
 
-function getLiaisonBySecteur($idSecteur) : array {
-    $resultat = array();
-
+function getLiaisonBySecteur(int $idSecteur) : array {
     try {
-    $cnx = connexionPDO();
-    $req = $cnx->prepare("SELECT * FROM liaison JOIN secteur ON liaison.codeSecteur = secteur.id  WHERE secteur.id = :idSecteur ORDER BY secteur.nom ");
-    $req->bindValue(':idSecteur', $idSecteur, PDO::PARAM_INT);
-    $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
-    } 
-    catch (PDOException $e) {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("SELECT * FROM liaison JOIN secteur ON liaison.codeSecteur = secteur.id  WHERE secteur.id = :idSecteur ORDER BY secteur.nom");
+        $req->bindValue(':idSecteur', $idSecteur, PDO::PARAM_INT);
+
+        $req->execute();
+
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
     }
-    return $resultat ;
+    return $resultat;
 }
 
 function getLiaison() : array {
@@ -41,6 +37,8 @@ function getLiaison() : array {
     }
     return $resultat ;
 }
+
+
 
 $includes = get_included_files();
 ?>
