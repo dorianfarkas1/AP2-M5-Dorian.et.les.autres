b61f37d1c6b99869742091c8fc15595a04bc9de4
Judith hedible <judith.hedible@clsi.fr>
2023-04-23 15:45:51 +0200

    ll

--- Notre site/Controleur/CTRcrudBateau.php
+++ Notre site/Controleur/CTRcrudBateau.php
@@ -5,57 +5,6 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
 include_once "$racine/Modele/bd.bateau.inc.php";
 
 // recuperation des donnees GET, POST et SESSION
-if(isset ($_POST['add']))
-{
-    $nom = htmlentities($_POST['nom']);
-    
-    $resultat = ajouterBateau($nom);
-
-    if($resultat)
-    {
-        $_SESSION["success"] = 'Bateau ajouté';
-    }
-    else
-    {
-        $_SESSION["error"] = 'Problème lors de l\'ajout du bateau';
-    }
-}
-
-if(isset($_POST['edit'])){
-    $id = htmlentities($_POST['id'];)
-    $nom = htmlentities($_POST['nom']);
-    $photoName = htmlentities($_POST['old_photo']);
-    if(isset($_FILES['photo'])){
-        unlink('./images/bateaux/'.$photoName);
-        $tmpName = $_FILES['photo']['tmp_name'];
-        $photoName = $_FILES['photo']['name'];
-        move_uploaded_file($tmpName, './images/bateaux/'.$photoName);
-    }
-    $resultat = modifierBateau($id, $nom, $photoName);
-
-    if($resultat){
-        $_SESSION['success'] = 'Bateau modifié';
-    }		
-    else{
-        $_SESSION['error'] = 'Problème lors de la modification du bateau';
-    }
-}
-
-if(isset($_POST['supr'])){
-    $id = htmlentities($_POST['id']);
-    $photoName = htmlentities($_POST['old_photo']);
-    if ($photoName != ""){
-           unlink('./images/bateaux/'.$photoName);
-    }
-    $resultat = supprimerBateau($id);
-    
-    if($resultat){
-        $_SESSION['success'] = 'Bateau supprimé';
-    }		
-    else{
-        $_SESSION['error'] = 'Problème lors de la suppression du bateau';
-    }
-}
 
 // appel des fonctions permettant de recuperer les donnees utiles a l'affichage 
 $lesBateaux = getBateau();
