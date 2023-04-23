c9f34b80834bc96f01d882412ed29e35e9d5a38f
Judith hedible <judith.hedible@clsi.fr>
2023-04-23 16:16:32 +0200

    bro

--- Notre site/Vue/crudBateau/add_modal.php
+++ Notre site/Vue/crudBateau/add_modal.php
@@ -6,7 +6,7 @@
             <h5 class="modal-title">Ajouter un nouveau bateau</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form method="POST" action="?action=modifieBateau" enctype="multipart/form-data">
+        <form method="POST" action="?action=bateauTraitement" enctype = "multipart/form-data">
             <div class="modal-body">
                 <div class="row form-group">
 					<div class="col-sm-2">
@@ -16,100 +16,147 @@
 						<input type="text" class="form-control" name="nom" required>
 					</div>
 				</div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Photo:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <input type="file" class="form-control" name="photo">
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Description:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <textarea class="form-control" name="description" required></textarea>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Longueur:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <input type="number" step="0.1" class="form-control" name="longueur" min="0" required>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Largeur:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <input type="number" step="0.1" class="form-control" name="largeur" min="0" required>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Vitesse:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <input type="number" class="form-control" step="1" min="0" name="vitesse" required>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Niveau PMR:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <select name="PMR" class="form-control" required>
-                            <option value="">--- Choisissez un niveau de PMR ---</option>
-                            <?php
-                                $stmt1 = $connexion->prepare('SELECT * FROM niveau_accessibilite');
-                                $stmt1->execute(array());
-                                $lesNiveauPMRs = $stmt1->fetchAll(PDO::FETCH_ASSOC);
-                                foreach($lesNiveauPMRs as $unNiveauPMR) {
-                            ?>
-                            <option value="<?= $unNiveauPMR["idNiveau"] ?>"><?= ucfirst($unNiveauPMR["libelle"]) ?></option>
-                            <?php } ?>
-                        </select>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-2">
-                        <label class="control-label modal-label">Secteur:</label>
-                    </div>
-                    <div class="col-sm-10">
-                        <fieldset class="form-control">
-                            <?php
-                            $SQL = "SELECT * FROM secteur";
-                            $stmt2 = $connexion->prepare($SQL);
-                            $stmt2->execute();
-                            $lesSecteurs = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
-                            foreach ($lesSecteurs as $unSecteur) {
-                            ?>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">Photo:</label>
+					</div>
+					<div class="col-sm-10">
+						<input type="file" class="form-control" name="photo" >
+					</div>
+				</div>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">Dscpt°:</label>
+					</div>
+					<div class="col-sm-10">
+                    <textarea class="form-control" name="description" required><?= $row['description'] ?>></textarea>
+					</div>
+				</div>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">Longueur:</label>
+					</div>
+					<div class="col-sm-10">
+                    <input type="number" step="0.1" class="form-control" name="longueur" min="0" value="<?= $row['longueur']?>" required>
+					</div>
+				</div>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">Largeur:</label>
+					</div>
+					<div class="col-sm-10">
+                    <input type="number" step="0.1" class="form-control" name="largeur" min="0" value="<?= $row['largeur']?>" required>
+					</div>
+				</div>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">Vitesse_C:</label>
+					</div>
+					<div class="col-sm-10">
+                    <input type="number" class="form-control" step="1" min="0" name="vitesse" value="<?= $row['vitesse_croisiere']?>"  required>
+					</div>
+				</div>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">NivPMR:</label>
+					</div>
+					<div class="col-sm-10">
+                        <select name="PMR" class="form-control" required> 
+                            <option value="">--- Choisissez un niveau de PMR ---</option>
+                                <?php 
+                                        $stmt1 = $connexion->prepare('SELECT * FROM niveau_accessibilite'); 
+                                        $stmt1->execute(array()); 
+                                        $lesNiveauPMRs = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+                                    foreach($lesNiveauPMRs as $unNiveauPMR) 
+                                    {
+                                        $selected = "";
+
+                                        if ($unNiveauPMR['idNiveau'] == $row['niveauPMR']){
+                                        $selected = "selected";
+                                        }
+                                     ?> 
+                                    <option value="<?= $unNiveauPMR["idNiveau"] ?>" <?= $selected ?>><?= ucfirst($unNiveauPMR["libelle"]) ?></option>
+                                    
+                              <?php } ?> 
+                        </select>
+					</div>
+				</div>
+                <div class="row form-group">
+					<div class="col-sm-2">
+						<label class="control-label modal-label">Secteur:</label>
+					</div>
+					<div class="col-sm-10">
+                    <fieldset class="form-control">
+
+                        <?php
+
+                                $SQL = "SELECT * FROM secteur";
+                                $stmt2 = $connexion->prepare($SQL);
+                                $stmt2->execute();
+                                $lesSecteurs = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+                                
+                                $req = $connexion->prepare('SELECT idSecteur FROM bateau_secteur WHERE idBAteau = :id');
+                                $req->bindParam(':id', $row['id'], PDO::PARAM_INT);
+                                $req->execute();
+                                $lesSecteursDuBateau = $req->fetchAll(PDO::FETCH_ASSOC); // le tableau $lesSecteursActuels contient les id des différents secteurs actuellement cochés pour le bateau concerné
+                             
+                                /* on prépare un tableau d'un seul niveau contenant les id des secteurs actuellement cochés */
+                                    $listeSecteurs = array();
+                                    foreach ($lesSecteursDuBateau as $UnSecteurDuBateau) {
+                                        array_push($listeSecteurs, $UnSecteurDuBateau['idSecteur']);
+                                    }
+                            foreach ($lesSecteurs as $unSecteur) 
+                            {
+                                $checked = "";
+                                if (in_array($unSecteur['id'], $listeSecteurs)){
+                                $checked = "checked";
+                                }
+                        ?>
+
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="secteurs[<?= $unSecteur["id"] ?>]"> 
+                                <input class="form-check-input" type="checkbox" name="secteurs[<?= $unSecteur["id"] ?>]"<?= $checked ?>>
                                 <label class="form-check-label" for="secteurs[<?= $unSecteur["id"] ?>]"><?= $unSecteur["nom"] ?></label>
                             </div>
-                            <?php } ?>
-                        </fieldset>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <div class="col-sm-10">
-                        <?php
+
+                        <?php } ?>
+
+                    </fieldset>
+					</div>
+
+                    <?php
+
+                            $req = $connexion->prepare('SELECT lettreCategorie, capaciteMax FROM contenance_bateau WHERE idBateau = :id');
+                            $req->bindParam(':id', $row['id'], PDO::PARAM_INT);
+                            $req->execute();
+                            $lesContenances = $req->fetchAll(PDO::FETCH_ASSOC); // le tableau $lesContenances contient les id des categories et du nb de places renseignées pour ce bateau
+                            /* on prépare un tableau avec comme clés les catégories et comme valeurs les nb de places */
+
+                            $nbPlacesParCategorie = array();
+
+                            foreach ($lesContenances as $uneContenance) {
+
+                               $nbPlacesParCategorie[$uneContenance['lettreCategorie']] = $uneContenance['capaciteMax'];
+                            }  
+                        
+                        $SQL = 'SELECT * FROM categorie';    
                         $stmt3 = $connexion->prepare('SELECT * FROM categorie');
-                        $stmt3->execute();
+                        $stmt3->execute(array());
                         $lesCategories = $stmt3->fetchAll(PDO::FETCH_ASSOC);
 
+                        ?>
+                        <div class="form-group">
+                        <?php
+
                         foreach ($lesCategories as $uneCategorie) { ?>
+
                         <label for="categories[<?= $uneCategorie["idCategorie"] ?>]">Nombre de <?= $uneCategorie["libelleCategorie"] ?> :</label>
-                        <input type="number" step="1" min="0" class="form-control" name="categories[<?= $uneCategorie["idCategorie"] ?>]" required>
+                        <input type="number" step="1" min="0" class="form-control" name="categories[<?= $uneCategorie["idCategorie"] ?>]" value="<?= $nbPlacesParCategorie[$uneCategorie["idCategorie"]] ?>" required>
+
                         <?php } ?>
-                    </div>
-                </div>
+				</div>
             </div>
+
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                 <i class="bi bi-x-circle"></i> Annuler
@@ -119,5 +166,6 @@
                 </button>
             </div>
         </form>
+        </div>
     </div>
 </div>
--- Notre site/Vue/crudBateau/edit_delete_modal.php
+++ Notre site/Vue/crudBateau/edit_delete_modal.php
@@ -6,10 +6,11 @@
             <h5 class="modal-title">Modifier un bateau</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form method="POST" action="?action=modifieBateau" enctype="multipart/form-data">
+        <form method="POST" action="?action=bateauTraitement">
             <div class="modal-body">
                 <div class="row form-group">
 					<input type="hidden" class="form-control" name="id" value="<?php echo $row['id']; ?>">
+
 					<div class="row form-group">
 						<div class="col-sm-2">
 							<label class="control-label modal-label">Nom:</label>
@@ -18,7 +19,9 @@
 							<input type="text" class="form-control" name="nom" value="<?php echo $row['nom']; ?>">
 						</div>
 					</div>
-                    <?php if ($row['photo']!=""){  ?>
+
+                    <?php if ($row['photo']!=""){
+                    ?>
                         <div class="row form-group">
                             <div class="col-sm-10">
                             <img height='100px' src='images/bateaux/<?= $row['photo'] ?>'>
@@ -26,40 +29,16 @@
                         </div>
                     <?php
                     }
-                    ?>
-                    <input type="hidden" class="form-control" name="old_photo" value="<?php echo $row['photo']; ?>">
+                    ?> 
+                    <input type="hidden" name="old_photo" class="form-control" value="<?php echo $row['photo']; ?>">
                     <div class="row form-group">
-                        <div class="col-sm-2">
-                            <label class="control-label modal-label">Photo:</label>
-                        </div>
-                        <div class="col-sm-10">
-                            <input type="file" class="form-control" name="photo">
-                        </div>
-                    </div>
-                    <div class="row form-group">
-                            <div class="col-sm-2">
-                                <label class="control-label modal-label">Description :</label>
-                            </div>
-                            <div class="col-sm-10">
-                                <textarea name="description" class="form-control" required rows="5" cols="33"><?php echo $row['description']; ?></textarea>
-                            </div>
-                    </div>
-                    <div class="row form-group">
-                            <div class="col-sm-2">
-                                <label class="control-label modal-label">Longueur :</label>
-                            </div>
-                            <div class="col-sm-10">
-                                <textarea name="description" class="form-control" required rows="5" cols="33"><?php echo $row['longueur']; ?></textarea>
-                            </div>
-                    </div>
-                    <div class="row form-group">
-                            <div class="col-sm-2">
-                                <label class="control-label modal-label">Largeur :</label>
-                            </div>
-                            <div class="col-sm-10">
-                                <textarea name="description" class="form-control" required rows="5" cols="33"><?php echo $row['largeur']; ?></textarea>
-                            </div>
-                    </div>
+						<div class="col-sm-2">
+							<label class="control-label modal-label">Photo:</label>
+						</div>
+						<div class="col-sm-10">
+							<input type="file" class="form-control" name="photo" >
+						</div>
+					</div>
 				</div>
             </div>
             <div class="modal-footer">
@@ -85,12 +64,12 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
             <div class="modal-body">
-				<p class="text-center">Etes-vous sûr de vouloir supprimer le bateau <?php echo $row['nom']; ?> ?</p>
+				<p class="text-center">Etes-vous sure de vouloir supprimer le bateau <?php echo $row['nom']; ?></p>
             </div>
             <div class="modal-footer">
-				<form method="POST" action="?action=modifieBateau">
+				<form method="POST" action="?action=bateauTraitement">
 					<input type="hidden" class="form-control" name="id" value="<?php echo $row['id']; ?>">
-                    <input type="hidden" class="form-control" name="old_photo" value="<?php echo $row['photo']; ?>">
+                    <input type="hidden" name="old_photo" class="form-control" value="<?php echo $row['photo']; ?>">
 					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
 						<i class="bi bi-x-circle"></i> Annuler
 					</button>
