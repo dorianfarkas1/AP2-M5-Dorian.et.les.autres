cf997ba4aa296833aa5d7a5f1a62ddf5ac8af14a
Judith hedible <judith.hedible@clsi.fr>
2023-04-30 12:52:24 +0200

    kik

--- Notre site/Vue/vueCrudbateau.php
+++ Notre site/Vue/vueCrudbateau.php
@@ -82,5 +82,10 @@
 $(document).ready(function(){
 	//inialize datatable
     $('#myTable').DataTable();
+
+	//hide alert
+	$(document).on('click', '.close', function(){
+        $('.alert').hide();
+	});
 });
 </script>
--- Notre site/Vue/vueCrudport.php
+++ Notre site/Vue/vueCrudport.php
@@ -73,5 +73,10 @@
 $(document).ready(function(){
 	//inialize datatable
     $('#myTable').DataTable();
+
+	//hide alert
+	$(document).on('click', '.close', function(){
+        $('.alert').hide();
+	});
 });
 </script>
