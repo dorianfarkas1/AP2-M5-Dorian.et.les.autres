0bcf73cb9b0500a2a403afe9199a49211f312187
Judith hedible <judith.hedible@clsi.fr>
2023-05-01 21:18:41 +0200

    kiko

--- Notre_site/Controleur/CTRtrajets.php
+++ Notre_site/Controleur/CTRtrajets.php
@@ -3,7 +3,7 @@
 if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
-
+include_once "$racine/Modele/bd.traversee.inc.php";
 
 // appel du script de vue qui permet de gerer l'affichage des donnees
 $title = "Affichage des Trajets";
